21f71cf38cbe5aed3fcfb247f44f7c084ef8c281
Anshul134 <anshulagrawal134@gmail.com>
2019-09-16 00:16:04 +0530

    completed home page layout. completed stream page banner working on stream page

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,45 @@
 import React, {Component} from 'react';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 //importing other components for rendering
 
 /*
     *Navbar.js for rendering navbar on each page
+    *Home.js for rendering the home page
 */
 
 import Navbar from './Navbar';
 import Home from './Home';
+import StreamSong from './StreamSong';
 
 class App extends Component {
     state = {
         musicPreviews : [
-            {'name': 'Mad World', 'artist': 'Gary Jules', 'album': 'Dony Darko', 'clipArt': 'mad_world.jpg'},
-            {'name': 'Mini Cooper', 'artist': 'Ammy Virk', 'album': 'Nikka Zildar', 'clipArt': 'mini_cooper.jpeg'},
-            {'name': 'Mad World', 'artist': 'Gary Jules', 'album': 'Dony Darko', 'clipArt': 'mad_world.jpg'},
-            {'name': 'Mini Cooper', 'artist': 'Ammy Virk', 'album': 'Nikka Zildar', 'clipArt': 'mini_cooper.jpeg'},
-            {'name': 'Mad World', 'artist': 'Gary Jules', 'album': 'Dony Darko', 'clipArt': 'mad_world.jpg'},
-            {'name': 'Mini Cooper', 'artist': 'Ammy Virk', 'album': 'Nikka Zildar', 'clipArt': 'mini_cooper.jpeg'},
-            {'name': 'Mad World', 'artist': 'Gary Jules', 'album': 'Dony Darko', 'clipArt': 'mad_world.jpg'},       
+            {'songId':1,'name': 'Mad World', 'artist': 'Gary Jules', 'album': 'Dony Darko', 'clipArt': 'mad_world.jpg', 'mood' : 'mellow'},
+            {'songId':2,'name': 'Mini Cooper', 'artist': 'Ammy Virk, Diljeet Dosanjh', 'album': 'Nikka Zildar', 'clipArt': 'mini_cooper.jpeg', 'mood' : 'romantic'},
+            {'songId':3,'name': 'Mad World', 'artist': 'Gary Jules', 'album': 'Dony Darko', 'clipArt': 'mad_world.jpg', 'mood' : 'mellow'},
+            {'songId':4,'name': 'Mini Cooper', 'artist': 'Ammy Virk', 'album': 'Nikka Zildar', 'clipArt': 'mini_cooper.jpeg', 'mood' : 'romantic'},
+            {'songId':5,'name': 'Mad World', 'artist': 'Gary Jules', 'album': 'Dony Darko', 'clipArt': 'mad_world.jpg', 'mood' : 'mellow'},
+            {'songId':6,'name': 'Mini Cooper', 'artist': 'Ammy Virk', 'album': 'Nikka Zildar', 'clipArt': 'mini_cooper.jpeg', 'mood' : 'romantic'},
+            {'songId':7,'name': 'Mad World', 'artist': 'Gary Jules', 'album': 'Dony Darko', 'clipArt': 'mad_world.jpg', 'mood' : 'mellow'},       
         ]
     }
     render() {
         return (
-            <div className='App'>
-                <Navbar />
-                <Home pageHead={'Music App'} musicPreviews={this.state.musicPreviews}/>
-            </div>
+            <Router>
+                <div className='App'>
+                    <Navbar />
+                    <Route path='/' exact={true}  render = {() => 
+                        <Home pageHead='Music App' musicPreviews ={this.state.musicPreviews}/>
+                    } />
+                    <Route path='/streamMusic/:id'   render = {({match}) => 
+                        <StreamSong songObj={this.state.musicPreviews.filter( (music) => 
+                            music.songId == match.params.id
+                        
+                        )}/>
+                        } />
+                    
+                </div>
+            </Router>                
         );
     }
 }
--- src/components/SongPreview.js
+++ src/components/SongPreview.js
@@ -1,26 +1,36 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import {Link, Route} from 'react-router-dom';
 
 class SongPreview extends Component {
     render() {
-        const {name, album, artist, clipArt} = this.props.songObj;
+        const {name, album, artist, clipArt, songId} = this.props.songObj;
         return(
             <div className='col-md-3 song-preview'>
                 <div className=''>
-                    <div className='song-preview-card'>
-                        <div className='clip-art'>
-														<img src = {`/assets/img/songs/${clipArt}`} className='song-preview-clipart' height='180'/>
- 												</div>
-												<div className="song-preview-name">
-													<p>{name}</p>
-												</div>
-												<div className="song-preview-artist">
-													<p>{artist}</p>
-												</div>
-                    </div>
+					
+                    <div className='song-preview-card' >
+												<Link to={`/streamMusic/${songId}`} >
+													<div className='clip-art'>
+															<img src = {`/assets/img/songs/${clipArt}`} className='song-preview-clipart' height='180'/>
+													</div>
+													<div className="song-preview-name">
+														<p>{name}</p>
+													</div>
+													<div className="song-preview-artist">
+														<p>{artist}</p>
+													</div>
+												</Link>	
+										</div>		
 									</div>    
             </div>
         )
     }
+};
+
+SongPreview.propTypes = {
+	songObj : PropTypes.object.isRequired,
 }
 
 export default SongPreview;
